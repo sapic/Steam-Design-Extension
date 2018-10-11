ea13bad12ed2fe1e5c48d9ac318c4f9476342800
The Oddball <oddball@oddball.tf>
2018-10-11 07:04:19 -0700

    add auto vernum

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var zip = require('gulp-zip');
 var del = require('del');
+var fs = require('fs');
 
 gulp.task('dir', function(){
     return gulp.src('*.*', {read: false})
@@ -12,7 +13,7 @@ gulp.task('dir', function(){
 gulp.task('js', function(){
     return gulp.src([
         './src/js/sapic-preview-button.js',
-        './src/js/jquery-3.2.1.min.js'
+        './src/js/jquery-3.3.1.min.js'
       ])
         .pipe(concat('main.js'))
         .pipe(uglify())
@@ -20,6 +21,11 @@ gulp.task('js', function(){
 });
 
 gulp.task('build', function(){
+    var content = fs.readFileSync('./src/manifest.json', {
+        encoding: 'utf-8'  
+    });
+    var vernum = content.match(/\.[0-9]{2,}/g)
+    fs.writeFileSync('./src/manifest.json', content.replace(vernum, (parseFloat(vernum) + 0.01).toFixed(2).replace(/^0+/, '')));
     return gulp.src([
         './out/build/main.js',
         './src/icon48.png',
