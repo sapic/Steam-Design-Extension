6731195a89a96753b5164fa73ac095335e82fdd9
The Oddball <oddball@oddball.tf>
2018-10-11 05:22:15 -0700

    redo gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-const zip = require('gulp-zip');
-var fs = require('fs');
+var zip = require('gulp-zip');
+var del = require('del');
+
+gulp.task('dir', function(){
+    return gulp.src('*.*', {read: false})
+        .pipe(gulp.dest('./out/build'))
+})
 
 gulp.task('js', function(){
-    gulp.src([
-        './src/js/main.js',
+    return gulp.src([
+        './src/js/sapic-preview-button.js',
         './src/js/jquery-3.2.1.min.js'
       ])
         .pipe(concat('main.js'))
@@ -14,13 +19,19 @@ gulp.task('js', function(){
         .pipe(gulp.dest('./out/build'))
 });
 
-gulp.task('default', ['js'], function() {
-    gulp.src([
+gulp.task('build', function(){
+    return gulp.src([
         './out/build/main.js',
         './src/icon48.png',
         './src/icon128.png',
-        './src/mainifest.json'
+        './src/manifest.json'
     ])
         .pipe(zip('Steam-Design-Extension.zip'))
         .pipe(gulp.dest('./out'))
-});
+})
+
+gulp.task('clean', function(){
+    return del('./out/build')
+})
+
+gulp.task('default', gulp.series('dir', 'js', 'build', 'clean'));
