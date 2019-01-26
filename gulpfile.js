93e9d177aa77fdb9afdcc5f447302a4e42618b49
The Oddball <oddball@oddball.tf>
2019-01-26 06:15:56 -0700

    Add firefox support

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,6 @@ gulp.task('js', function(){
         './src/js/sapic-preview-button.js',
         './src/js/jquery-3.3.1.min.js'
       ])
-        .pipe(concat('main.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./out/build'))
 });
@@ -27,7 +26,8 @@ gulp.task('build', function(){
     var vernum = content.match(/\.[0-9]{2,}/g)
     fs.writeFileSync('./src/manifest.json', content.replace(vernum, (parseFloat(vernum) + 0.01).toFixed(2).replace(/^0+/, '')));
     return gulp.src([
-        './out/build/main.js',
+        './out/build/jquery-3.3.1.min.js',
+        './out/build/sapic-preview-button.js',
         './src/icon48.png',
         './src/icon128.png',
         './src/manifest.json'
