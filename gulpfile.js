67fc71d377bd874650cd58b47128022f19fe938a
Tim <truecarry@gmail.com>
2020-10-16 09:28:05 +0300

    Some work on removing $

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-const { src, dest, parallel, series } = require('gulp');
-const uglify = require('gulp-uglify');
+const { src, dest, parallel, series, watch } = require('gulp');
+const terser = require('gulp-terser');
 const zip = require('gulp-zip');
 const del = require('del');
 const fs = require('fs');
@@ -11,23 +11,45 @@ function dir() {
 
 function js() {
     return src([
-            './src/js/sapic-preview-button.js',
-            './src/js/jquery-3.4.1.min.js'
-        ])
-        .pipe(uglify())
+        './src/js/sapic-preview-button.js',
+        './src/js/jquery-3.4.1.min.js'
+    ])
+        .pipe(terser())
+        .pipe(dest('./out/build'))
+}
+
+function jsDev() {
+    return src([
+        './src/js/sapic-preview-button.js',
+        './src/js/jquery-3.4.1.min.js'
+    ])
+        .pipe(terser())
         .pipe(dest('./out/build'))
 }
 
 function build() {
     return src([
-            './out/build/jquery-3.4.1.min.js',
-            './out/build/sapic-preview-button.js',
-            './src/icon48.png',
-            './src/icon128.png',
-            './src/manifest.json',
-            './designers.json'
-        ])
+        './out/build/jquery-3.4.1.min.js',
+        './out/build/sapic-preview-button.js',
+        './src/icon48.png',
+        './src/icon128.png',
+        './src/manifest.json',
+        './designers.json'
+    ])
         .pipe(zip('Steam-Design-Extension.zip'))
+        .pipe(dest('./out'))
+}
+
+function dev() {
+    return src([
+        './out/build/jquery-3.4.1.min.js',
+        './out/build/sapic-preview-button.js',
+        './src/icon48.png',
+        './src/icon128.png',
+        './src/manifest.json',
+        './designers.json',
+        "./src/assets/**",
+    ])
         .pipe(dest('./out'))
 }
 
@@ -48,3 +70,16 @@ exports.default = series(
     build,
     clean
 )
+
+exports.dev = function () {
+    const devPipeline = series(
+        dir,
+        jsDev,
+        dev,
+        clean
+    )
+
+    devPipeline()
+
+    watch('src/**', devPipeline)
+}
--- src/js/sapic-preview-button.js
+++ src/js/sapic-preview-button.js
@@ -1,6 +1,6 @@
 function longImageButton() {
     $('#title').after('<div class="long_sapic_button btn_blue_white_innerfade btn_medium" id="longImage" style="display:none;"><span>Upload a Long Image</span></div>');
-    $('#PreviewImage').on('load', function() {
+    $('#PreviewImage').on('load', function () {
         $('#longImage').show();
     });
 }
@@ -21,26 +21,30 @@ function scmSapicButton() {
 }
 
 function invSapicButton() {
-    $('#iteminfo1_item_icon').on('load', function() {
+    const iteminfo1_item_icon = document.getElementById('iteminfo1_item_icon')
+    iteminfo1_item_icon.onload = function () {
         invSapicButton_2(1);
-    });
-    $('#iteminfo0_item_icon').on('load', function() {
+    }
+
+    const iteminfo0_item_icon = document.getElementById('iteminfo0_item_icon')
+    iteminfo0_item_icon.onload = function () {
         invSapicButton_2(0);
-    });
+    }
 }
 
 function invSapicButton_2(id) {
-    var itemActions = $(".inventory_iteminfo").find(".item_desc_content").find(".item_desc_description").find("#iteminfo" + id + "_item_actions");
-    var viewFullButton = $(itemActions).find("a").first();
-    if (viewFullButton.length) {
-        var href = viewFullButton.attr('href');
-        var bgLink = /public\/images\/items/.test(href);
-        var httpLink = /cdn.akamai.steamstatic.com/.test(href);
+    const itemActions = document.getElementById(`iteminfo${id}_item_actions`)
+    const viewFullButton = itemActions.firstChild
+
+    if (isElement(viewFullButton)) {
+        let href = viewFullButton.getAttribute('href');
+        const bgLink = /public\/images\/items/.test(href);
+        const httpLink = /cdn.akamai.steamstatic.com/.test(href);
         if (httpLink) {
             href = href.replace("http://cdn.akamai.steamstatic.com", "https://steamcdn-a.akamaihd.net");
         }
-        if (bgLink && !(itemActions).find(".inv_sapic_button:not(#iteminfo" + id + "_item_actions)").length) {
-            viewFullButton.after('<a class="inv_sapic_button btn_small btn_grey_white_innerfade" target="_blank" href="https://steam.design/#' + href + '"><span>View on Steam.Design</span></a>');
+        if (bgLink) {// && !(itemActions).find(".inv_sapic_button:not(#iteminfo" + id + "_item_actions)").length) {
+            itemActions.innerHTML += '<a class="inv_sapic_button btn_small btn_grey_white_innerfade" target="_blank" href="https://steam.design/#' + href + '"><span>View on Steam.Design</span></a>'
         }
     }
 }
@@ -49,7 +53,7 @@ function settingsSapicButton() {
     var url = $('#profile_background_current_image').attr("src");
     if (url) {
         settingsSapicButton_2();
-        $('#profile_background_current_image').on('load', function() {
+        $('#profile_background_current_image').on('load', function () {
             settingsSapicButton_2();
         });
     }
@@ -70,56 +74,58 @@ function settingsSapicButton_2() {
 }
 
 function checkDesignerStatus() {
-    $.getJSON("https://raw.githubusercontent.com/sapic/Steam-Design-Extension/master/designers.json").done(function(data) {
-        $('body').append("<script id=\"idScript\">$J('#idScript').append('<param id=\"steamID\" value=\"'+ g_rgProfileData.steamid + '\">')</script>");
-        var id = $('#steamID').val()
+    fetch("https://raw.githubusercontent.com/sapic/Steam-Design-Extension/master/designers.json")
+        .then(r => r.json())
+        .then(function (data) {
+            $('body').append("<script id=\"idScript\">$J('#idScript').append('<param id=\"steamID\" value=\"'+ g_rgProfileData.steamid + '\">')</script>");
+            var id = $('#steamID').val()
 
-        $.each(data.designers, function() {
-            if (id == this) {
-                loadDesignerBanner("designer");
-            }
-        });
+            $.each(data.designers, function () {
+                if (id == this) {
+                    loadDesignerBanner("designer");
+                }
+            });
 
-        $.each(data.sapicstaff, function() {
-            if (id == this) {
-                loadDesignerBanner("sapic");
-            }
-        });
+            $.each(data.sapicstaff, function () {
+                if (id == this) {
+                    loadDesignerBanner("sapic");
+                }
+            });
 
-        $.each(data.aevoa, function() {
-            if (id == this) {
-                loadDesignerBanner("aevoa");
-            }
-        });
+            $.each(data.aevoa, function () {
+                if (id == this) {
+                    loadDesignerBanner("aevoa");
+                }
+            });
 
-        $.each(data.donator, function() {
-            if (id == this) {
-                loadDesignerBanner("donator");
-            }
+            $.each(data.donator, function () {
+                if (id == this) {
+                    loadDesignerBanner("donator");
+                }
+            });
         });
-    });
 
 }
 
 function loadDesignerBanner(banner) {
     switch (banner) {
         case "designer":
-            var imgurl = "https://i.oddball.tf/lvkAtUA.png";
+            var imgurl = chrome.runtime.getURL("images/designer.png");
             var href = "https://designerlist.guide";
             var text = "Verified Profile Designer";
             break;
         case "sapic":
-            var imgurl = "https://i.oddball.tf/egACnNm.png";
+            var imgurl = chrome.runtime.getURL("images/sapic.png");
             var href = "https://Steam.Design";
             var text = "Steam.Design Staff";
             break;
         case "aevoa":
-            var imgurl = "https://i.oddball.tf/KAnFF5k.png";
+            var imgurl = chrome.runtime.getURL("images/aevoa.png");
             var href = "https://steamcommunity.com/id/Aevoa/myworkshopfiles/?section=guides";
             var text = "150+ Guides Published";
             break;
         case "donator":
-            var imgurl = "https://i.oddball.tf/rjZpjUn.png";
+            var imgurl = chrome.runtime.getURL("images/donator.png");
             var href = "https://Steam.Design";
             var text = "Steam.Design Donator";
             break;
@@ -129,7 +135,7 @@ function loadDesignerBanner(banner) {
     $('.profile_in_game.persona').not($('.sapicbadge')).after(html);
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     $.ajaxSetup({ cache: false });
     var href = window.location.href;
     if (/\/market\/listings\/753\//.test(href)) {
@@ -144,9 +150,24 @@ $(document).ready(function() {
         checkDesignerStatus();
     }
 
-    $('.long_sapic_button').on('click', function() {
+    $('.long_sapic_button').on('click', function () {
         $('#image_width').val('1000');
         $('#image_height').val('1');
         $(this).hide();
     });
 });
+
+function isElement(obj) {
+    try {
+        //Using W3 DOM2 (works for FF, Opera and Chrome)
+        return obj instanceof HTMLElement;
+    }
+    catch (e) {
+        //Browsers not supporting W3 DOM2 don't have HTMLElement and
+        //an exception is thrown and we end up here. Testing some
+        //properties that all elements have (works on IE7)
+        return (typeof obj === "object") &&
+            (obj.nodeType === 1) && (typeof obj.style === "object") &&
+            (typeof obj.ownerDocument === "object");
+    }
+}
